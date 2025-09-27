44fbd5c322e0cb868befaa5963cb7d8edd5d9712
Disandu Rodrigo <you@example.com>
2025-09-27 17:17:36 +0530

    Initial commit

---
+++ src/java/controller/LoadData.java
@@ -0,0 +1,83 @@
+package controller;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import entity.Category;
+import entity.Color;
+import entity.Product;
+import entity.Product_Condition;
+import entity.Storage;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.HibernateUtil;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Order;
+
+@WebServlet(name = "LoadData", urlPatterns = {"/LoadData"})
+public class LoadData extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("success", false);
+
+        Gson gson = new Gson();
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        //main code
+        //get category list from db
+        Criteria criteria1 = session.createCriteria(Category.class);
+        List<Category> categoryList = criteria1.list();
+        jsonObject.add("categoryList", gson.toJsonTree(categoryList));
+
+        //get condition list from db
+        Criteria criteria2 = session.createCriteria(Product_Condition.class);
+        List<Product_Condition> conditionList = criteria2.list();
+        jsonObject.add("conditionList", gson.toJsonTree(conditionList));
+
+        //get color list from db
+        Criteria criteria3 = session.createCriteria(Color.class);
+        List<Color> colorList = criteria3.list();
+        jsonObject.add("colorList", gson.toJsonTree(colorList));
+
+        //get storage list from db
+        Criteria criteria4 = session.createCriteria(Storage.class);
+        List<Storage> storageList = criteria4.list();
+        jsonObject.add("storageList", gson.toJsonTree(storageList));
+
+        //get product list from db
+        Criteria criteria5 = session.createCriteria(Product.class);
+
+        //get latest product
+        criteria5.addOrder(Order.desc("id"));
+        jsonObject.addProperty("allProductCount", criteria5.list().size());
+
+        //set product range
+        criteria5.setFirstResult(0);
+        criteria5.setMaxResults(3);
+
+        List<Product> productList = criteria5.list();
+
+        //remove users in product
+        for (Product product : productList) {
+            product.setUser(null);
+        }
+
+        jsonObject.add("productList", gson.toJsonTree(productList));
+        jsonObject.addProperty("success", true);
+
+        //main code
+        response.setContentType("application/json");
+        response.getWriter().write(gson.toJson(jsonObject));
+
+    }
+
+}
---
+++ src/java/controller/SignUp.java
@@ -0,0 +1,113 @@
+package controller;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dto.Response_DTO;
+import dto.User_DTO;
+import entity.User;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.HibernateUtil;
+import model.Mail;
+import model.Validations;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
+@WebServlet(name = "SignUp", urlPatterns = {"/SignUp"})
+public class SignUp extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Response_DTO response_DTO = new Response_DTO();
+
+        Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+
+        User_DTO user_DTO = gson.fromJson(request.getReader(), User_DTO.class);
+
+        if (user_DTO.getFirst_name().isEmpty()) {
+
+            response_DTO.setContent("Please enter your First Name");
+
+        } else if (user_DTO.getLast_name().isEmpty()) {
+
+            response_DTO.setContent("Please enter your Last Name");
+
+        } else if (user_DTO.getEmail().isEmpty()) {
+
+            response_DTO.setContent("Please enter your Email Address");
+
+        } else if (!Validations.isEmailValid(user_DTO.getEmail())) {
+
+            response_DTO.setContent("Please enter valid Email Address");
+
+        } else if (user_DTO.getPassword().isEmpty()) {
+
+            response_DTO.setContent("Please enter your Password");
+
+        } else if (!Validations.isPasswordValid(user_DTO.getPassword())) {
+
+            response_DTO.setContent("Password must include at least one uppercase letter, "
+                    + "lowercase letter, "
+                    + "number, "
+                    + "special character, "
+                    + "minimum 8 characters");
+
+        } else {
+
+            Session session = HibernateUtil.getSessionFactory().openSession();
+
+            Criteria criteria1 = session.createCriteria(User.class);
+            criteria1.add(Restrictions.eq("email", user_DTO.getEmail()));
+
+            if (!criteria1.list().isEmpty()) {
+
+                response_DTO.setContent("User with this email already exists.");
+            } else {
+
+                //generate verification
+                int code = (int) (Math.random() * 1000000);
+
+                final User user = new User();
+                user.setEmail(user_DTO.getEmail());
+                user.setFirst_name(user_DTO.getFirst_name());
+                user.setLast_name(user_DTO.getLast_name());
+                user.setPassword(user_DTO.getPassword());
+                user.setVerification(String.valueOf(code));
+
+                //send verification email
+                Thread sendMailThread = new Thread() {
+
+                    @Override
+                    public void run() {
+
+                        Mail.sendMail(user.getEmail(), "Smart Trade Verification","<h1 style=\"color:#6482AD;\">Your Verification Code:" + user.getVerification() + "</h1>");
+
+                    }
+
+                };
+                sendMailThread.start();
+
+                session.save(user);
+                session.beginTransaction().commit();
+
+                request.getSession().setAttribute("email", user_DTO.getEmail());
+                response_DTO.setSuccess(true);
+                response_DTO.setContent("Registration Complete. Please check your inbox for Verification!");
+            }
+
+            session.close();
+
+        }
+
+        response.setContentType("application/json");
+        response.getWriter().write(gson.toJson(response_DTO));
+
+    }
+
+}
